430c1653b35f8ec0cbe7cf563273e3cd1c71a7e1
Diki Armanda <diki.armada5@gmail.com>
2023-11-29 20:18:31 +0700

    fix bug & split components

--- catatan-belanja/src/App.jsx
+++ catatan-belanja/src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useState } from "react"
+import Header from "./components/Header"
+import Form from "./components/Form"
+import GroceryList from "./components/GroceryList"
+import Footer from "./components/Footer"
 
 const groceryItems = [
   {
@@ -25,94 +29,27 @@ export default function App() {
   const [items, setItems] = useState(groceryItems)
 
   function handleAddItem(item) {
-    setItems(...items, item)
+    setItems([...items, item])
   }
 
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id))
   }
 
+  function handleToggleItem(id) {
+    setItems((items) => items.map((item) => (item.id === id ? { ...item, checked: !item.checked} : item)))
+  }
+
+  function handleClearItems() {
+    setItems([])
+  }
+
   return (
     <div className="app">
       <Header />
       <Form onAddItem={handleAddItem}/>
-      <GroceryList items={items} onDeleteItem={handleDeleteItem}/>
-      <Footer />
+      <GroceryList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearItems={handleClearItems}/>
+      <Footer items={items}/>
     </div>
-  );
-}
-
-function Header() {
-  return <h1>Catatan Belanjaku 📝</h1>;
-}
-
-function Form({onAddItem}) {
-  const [name, setName] = useState('')
-  const [quantity, setQuantity] = useState(1)
-
-  function handleSubmit(e) {
-    e.preventDefault()
-
-    if (!name) return
-
-    const newItem = {
-      name,
-      quantity,
-      checked: false,
-      id: Date.now()
-    }
-    onAddItem(newItem)
-
-    setName('')
-    setQuantity(1)
-  }
-
-  const quantityNum = [...Array(20)].map((_, i) => (
-    <option value={i+1} key={i+1}>{i+1}</option>
-  ))
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>Hari ini belanja apa kita?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
-      <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
-      <button>Tambah</button>
-    </form>
-  );
-}
-
-function GroceryList({items, onDeleteItem}) {
-  return (
-    <>
-      <div className="list">
-        <ul>
-          {items.map((item) => (
-            <Item item={item} key={item.id} onDeleteItem={onDeleteItem}/>
-          ))}
-        </ul>
-      </div>
-      <div className="actions">
-        <select>
-          <option value="input">Urutkan berdasarkan urutan input</option>
-          <option value="name">Urutkan berdasarkan nama barang</option>
-          <option value="checked">Urutkan berdasarkan ceklis</option>
-        </select>
-        <button>Bersihkan Daftar</button>
-      </div>
-    </>
-  );
-}
-
-function Item({item, onDeleteItem}) {
-  return (
-    <li key={item.id}>
-      <input type="checkbox" />
-      <span style={item.checked ? { textDecoration: 'line-through' } : {}}>{item.quantity} {item.name}</span>
-      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
-    </li>
   )
-}
-
-function Footer() {
-  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
 }
