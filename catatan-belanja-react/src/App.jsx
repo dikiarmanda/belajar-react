aeca0ed386f2f34ad2be65725a311629adca2066
Diki Armanda <diki.armada5@gmail.com>
2023-12-01 17:39:07 +0700

    break component

--- catatan-belanja-react/src/App.jsx
+++ catatan-belanja-react/src/App.jsx
@@ -1,60 +1,66 @@
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
 export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <GroceryList />
+      <Footer />
+    </div>
+  );
+}
 
-  const groceryItems = [
-    {
-      id: 1,
-      name: 'Kopi Bubuk',
-      quantity: 2,
-      checked: true,
-    },
-    {
-      id: 2,
-      name: 'Gula Pasir',
-      quantity: 5,
-      checked: false,
-    },
-    {
-      id: 3,
-      name: 'Air Mineral',
-      quantity: 3,
-      checked: false,
-    },
-  ]
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
 
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <input type="text" placeholder="nama barang..." />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function GroceryList() {
   return (
     <>
-      <h1>Catatan Belanjaku 📝</h1>
-      <form className="add-form">
-        <h3>Hari ini belanja apa kita?</h3>
-        <div>
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <input type="text" placeholder="nama barang..." />
-        </div>
-        <button>Tambah</button>
-      </form>
       <div className="list">
         <ul>
-          <li>
-            <input type="checkbox" checked="true" />
-            <span>1 Kopi</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>5 Gula Pasir</span>
-            <button>&times;</button>
-          </li>
-          <li>
-            <input type="checkbox" />
-            <span>3 Air Mineral</span>
-            <button>&times;</button>
-          </li>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id}/>
+          ))}
         </ul>
       </div>
       <div className="actions">
@@ -65,9 +71,26 @@ export default function App() {
         </select>
         <button>Bersihkan Daftar</button>
       </div>
-      <footer className="stats">
-        Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
-      </footer>
     </>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" checked={item.checked} />
+      <span style={item.checked ? {  textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>&times;</button>
+    </li>
   )
+}
+
+function Footer() {
+  return (
+    <footer className="stats">
+      Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+    </footer>
+  );
 }
