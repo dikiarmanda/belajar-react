bdc0e1961a888f82cc53a736195f0ccef50bc22e
Diki Armanda <diki.armada5@gmail.com>
2023-12-01 18:28:45 +0700

    set state

--- catatan-belanja-react/src/App.jsx
+++ catatan-belanja-react/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const groceryItems = [
   {
     id: 1,
@@ -35,17 +37,23 @@ function Header() {
 }
 
 function Form() {
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
 
   const quantityNum = [...Array(20)].map((_, i) => (
     <option value={i+1} key={i+1}>{i+1}</option>
   ))
 
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>{quantityNum}</select>
-        <input type="text" placeholder="nama barang..." />
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>{quantityNum}</select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
       </div>
       <button>Tambah</button>
     </form>
