35963572bca9ff349b43ecd9faa0fc8dd242fc03
Diki Armanda <diki.armada5@gmail.com>
2023-12-01 18:40:26 +0700

    handleSubmit

--- catatan-belanja-react/src/App.jsx
+++ catatan-belanja-react/src/App.jsx
@@ -42,6 +42,18 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault()
+
+    if (!name) return
+
+    const newItem = {
+      name,
+      quantity,
+      checked: false,
+      id: Date.now()
+    }
+
+    setName('')
+    setQuantity(1)
   }
 
   const quantityNum = [...Array(20)].map((_, i) => (
@@ -52,7 +64,7 @@ function Form() {
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>{quantityNum}</select>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>{quantityNum}</select>
         <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
       </div>
       <button>Tambah</button>
