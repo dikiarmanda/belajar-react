938acc67f677792048f9fb1628e7e5a5e9301766
Diki Armanda <diki.armada5@gmail.com>
2023-12-01 18:48:04 +0700

    lifting state up

--- catatan-belanja-react/src/App.jsx
+++ catatan-belanja-react/src/App.jsx
@@ -22,11 +22,17 @@ const groceryItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(groceryItems)
+
+  function handleAddItem(item) {
+    setItems([...items, item])
+  }
+
   return (
     <div className="App">
       <Header />
-      <Form />
-      <GroceryList />
+      <Form onAddItem={handleAddItem}/>
+      <GroceryList items={items}/>
       <Footer />
     </div>
   );
@@ -36,7 +42,7 @@ function Header() {
   return <h1>Catatan Belanjaku 📝</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [name, setName] = useState('')
   const [quantity, setQuantity] = useState(1)
 
@@ -51,6 +57,7 @@ function Form() {
       checked: false,
       id: Date.now()
     }
+    onAddItem(newItem)
 
     setName('')
     setQuantity(1)
@@ -72,12 +79,12 @@ function Form() {
   );
 }
 
-function GroceryList() {
+function GroceryList({ items }) {
   return (
     <>
       <div className="list">
         <ul>
-          {groceryItems.map((item) => (
+          {items.map((item) => (
             <Item item={item} key={item.id}/>
           ))}
         </ul>
